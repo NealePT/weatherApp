be89675a1d1354671b2e1b41f24395b5a78ff311
Neale Taylor <nealeptaylor@gmail.com>
2022-09-20 13:13:27 -0400

    add styling to new components

--- weather-app/src/components/weather.js
+++ weather-app/src/components/weather.js
@@ -5,18 +5,19 @@ import moment from 'moment';
 const Weather = ({weatherData}) => {
   return (
     <div className='main'>
-      <p className='header'>City Name: {weatherData.name}</p>
+      <p className='header'>{weatherData.name}</p>
       <div className='flex'>
-        <p className='day'>Day: {moment().format('dddd')}</p>
-        <p className='day'>{moment().format('LL')}</p>
+      <p className='day'>{moment().format('dddd')}, <span>{moment().format('LL')}</span></p>
+      <p className='description'>{weatherData.weather[0].main}</p>
       </div>
       <div className='flex'>
         <p className='temp'>Temprature: {weatherData.main.temp} &deg;C</p>
         <p className='temp'>Humidity: {weatherData.main.humidity} %</p>
       </div>
-      {/* <p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p> */}
-      {/* <p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p> */}
-      {/* <p>Description: {weatherData.weather[0].main}</p> */}
+      <div className='flex'>
+        <p className='sun'>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
+        <p className='sun'>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
+      </div>
     </div>
   )
 };
