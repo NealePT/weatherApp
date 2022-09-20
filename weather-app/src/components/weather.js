cc02fa829e919b150f0ad9eb08e80feea1eeebb1
Neale Taylor <nealeptaylor@gmail.com>
2022-09-20 13:08:20 -0400

    reorganize fields"

--- weather-app/src/components/weather.js
+++ weather-app/src/components/weather.js
@@ -6,17 +6,17 @@ const Weather = ({weatherData}) => {
   return (
     <div className='main'>
       <p className='header'>City Name: {weatherData.name}</p>
-      <div>
+      <div className='flex'>
         <p className='day'>Day: {moment().format('dddd')}</p>
+        <p className='day'>{moment().format('LL')}</p>
       </div>
-      <div>
+      <div className='flex'>
         <p className='temp'>Temprature: {weatherData.main.temp} &deg;C</p>
+        <p className='temp'>Humidity: {weatherData.main.humidity} %</p>
       </div>
       {/* <p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p> */}
       {/* <p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p> */}
       {/* <p>Description: {weatherData.weather[0].main}</p> */}
-      {/* <p>Humidity: {weatherData.main.humidity} %</p> */}
-      {/* <p>Date: {moment().format('LL')}</p> */}
     </div>
   )
 };
