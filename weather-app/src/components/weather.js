a84a05da22d5e81944bb792444c375272d5f5dfa
Neale Taylor <nealeptaylor@gmail.com>
2022-09-20 13:32:06 -0400

    add refresh button

--- weather-app/src/components/weather.js
+++ weather-app/src/components/weather.js
@@ -2,18 +2,25 @@ import React from 'react';
 import '../styles.css'
 import moment from 'moment';
 
+const refresh = () => {
+  window.location.reload();
+}
+
 const Weather = ({weatherData}) => {
   return (
     <div className='main'>
-      <p className='header'>{weatherData.name}</p>
+      <div className='top'>
+        <p className='header'>{weatherData.name}</p>
+        <aside className='button' onClick={refresh}><i class="fa-solid fa-arrows-rotate"></i></aside>
+      </div>
       <div className='flex'>
-      <p className='day'>{moment().format('dddd')}, <span>{moment().format('LL')}</span></p>
-      <p className='description'>{weatherData.weather[0].main}</p>
+        <p className='day'>{moment().format('dddd')}, <span>{moment().format('LL')}</span></p>
+        <p className='description'>{weatherData.weather[0].main}</p>
       </div>
       <div className='flex'>
         <p className='temp'>Temprature: {weatherData.main.temp} &deg;C</p>
         <p className='temp'>Humidity: {weatherData.main.humidity} %</p>
-      </div>
+      </div>å
       <div className='flex'>
         <p className='sun'>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
         <p className='sun'>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
