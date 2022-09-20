6e3db97c8000809540a893d5223b76356de139d5
Neale Taylor <nealeptaylor@gmail.com>
2022-09-20 11:36:17 -0400

    add constants for lat and long

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
+import React, { useEffect, useState } from "react";
+
 
 function App() {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+
   return (
     <div className="App">
 
